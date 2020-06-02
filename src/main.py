8817defe8945dba86eb55ba04e76623c8fc0abf6
Andrew McCann <mccann.an@husky.neu.edu>
2020-06-02 11:59:16 -0400

    Generate the correct URL to scrape

--- src/main.py
+++ src/main.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python3
 BASE_URL = 'https://blackironbeast.com/5/3/1/calculator'
 
-def _get_config():
-  pass
-
-
-def _get_data(config):
-  pass
-
-
-def _to_csv(data):
-  pass
-
+def _generate_cycle(press, bench_press, squat, deadlift):
+  url = (f'https://blackironbeast.com/5/3/1/calculator?'
+          'assistanceWork=boringButBig&massUnits=pounds&maxesSwitch=trainingMax'
+          '&militaryTrainingMaxWeight={press}&benchTrainingMaxWeight={bench_press}&squatTrainingMaxWeight={squat}&deadliftTrainingMaxWeight={deadlift}'
+          '&barWeight=45&platePairs_100=0&platePairs_55=0&platePairs_45=1&platePairs_35=1&platePairs_25=1&platePairs_20=0'
+          '&platePairs_15=0&platePairs_10=1&platePairs_7_5=0&platePairs_5=2&platePairs_2_5=1&platePairs_2=0&platePairs_1_5=0'
+          '&platePairs_1_25=0&platePairs_1=0&platePairs_0_75=0&platePairs_0_5=0&platePairs_0_25=0&warmup=defranco'
+          '&programming=fresher&weeksOrder=531D&order=normal&extraSets=noneExtraSets&firstSetLastSetsSelect=3&firstSetLastRepsSelect=5'
+          '&boringButBigOption=across50&boringButBigSwapOption=lessBoring&daysPerWeekOption=4&fullBodyPhaseOption=1'
+          '&fullBodyFullBoringSquatOption=65x5_75x5_85x5&fullBodyFullBoringBenchOption=65x5_75x5_85x5'
+          '&fullBodyFullBoringDeadliftOption=65x3_75x3_85x3&boringButBig3MonthChallengeMonthOption=1&buildingTheMonolithWaveOption=1'
+          '&youngJimWendlerSeasonOption=offseason&deload=1&boringButBigDeloadOption=3x10x50')
+  print(url)
 
 def main():
-  config = _get_config()
-  data = _get_data(config)
-  csv = _to_csv(data)
-  # with open(config['output'], 'w') as output_file:
-  #   output_file.write(csv)
+  weeks = _generate_cycle(120, 210, 240, 280)
 
 main()
